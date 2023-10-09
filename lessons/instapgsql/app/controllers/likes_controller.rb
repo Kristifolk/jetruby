d20b3bb884c87004d2af370a748bbc92d2c1d78a
Kristifolk <kristin.kar13@gmail.com>
2023-10-09 13:30:51 +0300

    add insta pgsql

---
+++ lessons/instapgsql/app/controllers/likes_controller.rb
@@ -0,0 +1,105 @@
+class LikesController < ApplicationController
+  before_action :set_like, only: %i[ show edit update destroy ]
+
+  # GET /likes or /likes.json
+  def index
+    @likes = Like.all
+  end
+
+  # GET /likes/1 or /likes/1.json
+  def show
+  end
+
+  
+    def check_like     
+    @like = Like.find_by user_id: current_user.id, post_id: params[:id]
+
+        if @like.nil?
+            params[:like] = true
+            params[:user_id] = current_user.id
+            params[:post_id] = params[:id]
+            create
+        else
+            params[:like] = @like[:like] == true ? nil : true
+            params[:user_id] = @like[:user_id]
+            params[:post_id] = @like[:post_id]
+            update
+        end
+    end
+
+    def check_dislike     
+    @like = Like.find_by user_id: current_user.id, post_id: params[:id]
+    
+        if @like.nil?
+            params[:like] = true
+            params[:user_id] = current_user.id
+            params[:post_id] = params[:id]
+            create
+         else
+            params[:like] = @like[:like] == false ? nil : false
+            params[:user_id] = @like[:user_id]
+            params[:post_id] = @like[:post_id]
+            update
+         end
+    end
+
+  # GET /likes/new
+  def new
+    @like = Like.new
+  end
+
+  # GET /likes/1/edit
+  def edit
+  end
+
+  # POST /likes or /likes.json
+  def create
+    @like = Like.new(like_params)
+
+    respond_to do |format|
+      if @like.save
+        format.html { redirect_to root_path, notice: "Like was successfully created." }
+        format.json { render :show, status: :created, location: @like }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @like.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /likes/1 or /likes/1.json
+  def update
+    respond_to do |format|
+      if @like.update(like_params)
+        format.html { redirect_to root_path, notice: "Like was successfully updated." }
+        format.json { render :show, status: :ok, location: @like }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @like.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /likes/1 or /likes/1.json
+  def destroy
+    @like.destroy
+
+    respond_to do |format|
+      format.html { redirect_to likes_url, notice: "Like was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_like
+      @like = Like.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def like_params
+      #params.require(:like).permit(:like)
+      params.permit(:like, :user_id, :post_id)
+
+    end
+end
---
+++ lessons/instapgsql/app/models/friend.rb
@@ -0,0 +1,3 @@
+class Friend < ApplicationRecord
+    has_many :user#у одного юзера много друзей
+end
